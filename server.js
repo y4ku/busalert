101219d101b87681c05b35809b5ae4cd37c612ef
Jakub Misterka <san.y4ku@gmail.com>
2016-07-30 12:44:14 -0500

    azure pathname

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
 var port = process.env.port;
+var path = require("path");
 
-app.get('/', function (req, res) {
-  res.sendFile('index.html');
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
 });
 
 app.listen(port, function () {
