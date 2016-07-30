ee1adfa497e1e559e60baba548ac3c71fa9bfda8
Jakub Misterka <san.y4ku@gmail.com>
2016-07-30 12:37:16 -0500

    proper port

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
+var port = process.env.port;
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(port, function () {
+  console.log('Example app listening on port: ' + port);
 });
